dc3955a5e83700af33fa415c5dfd10649fdc62ed
Thuc Nguyen <thucn2@uci.edu>
2022-04-18 22:25:49 -0700

    GPUS-51 Added cors and helmet packages

--- backend/server_apis.js
+++ backend/server_apis.js
@@ -4,13 +4,33 @@ if (config.error)
   console.error('Failed to load environmental variables');
   throw config.error;
 }
-console.log(process.env);
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const helmet = require('helmet');
 const fileSystem = require('fs');
 const path = require('path');
+const app = express();
 const port = 3000;
+const whitelistedDomains = process.env.WHITELISTED_DOMAINS.split(' ');
+const corsOption = 
+{
+  origin: function(origin, callback)
+  {
+    console.log('origin: ', origin);
+    if (whitelistedDomains.indexOf(origin) !== -1 || !origin)
+    {
+      callback(null, true);
+    }
+    else 
+    {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
 
+app.use(cors(corsOption));                        // cross-origin resource sharing
+app.use(helmet());                                // Secures app, setting HTTP headers
+app.options('*', cors())                          // include before other routes
 app.use(express.json());                          // for parsing application/json
 app.use(express.urlencoded({ extended: true }));  // for parsing application/x-www-form-urlencoded
 
@@ -41,23 +61,28 @@ function requestErrorHandler(err, req, res, next)
   jsonError['message'] = err;
   res.json(jsonError);
 }
-// ========================================================
 
+// ========================================================
 // Pre-HTTP Request Middleware
+// ========================================================
 app.use(logger);
 
 // Go through all files under apis and serve the endpoints
 const fileArray = fileSystem.readdirSync(process.env.PATH_APIS).map(file => path.join(process.env.PATH_APIS, file));
 const apiArray = [].concat.apply([],fileArray.map(filePath => {return require(filePath)}));
+// ********************************************************
+// TODO: Remove after everything's done
+console.log(process.env);
+console.log(whitelistedDomains);
 console.log(`File Array:`);
 console.log(fileArray);
 console.log(`API Array:`);
 console.log(apiArray);
-// TODO: Remove after everything's done
 app.get('/', (req, res) =>
 {
   res.send('API endpoints have loaded.');
 });
+// ********************************************************
 for (let i = 0; i < apiArray.length; i++)
 {
     const arrayObj = apiArray[i];
@@ -93,7 +118,9 @@ for (let i = 0; i < apiArray.length; i++)
     }, processResponse);
 }
 
+// ========================================================
 // Post-HTTP Request Middleware
+// ========================================================
 app.use(requestErrorHandler);
 
 // Start the server on the specified port
