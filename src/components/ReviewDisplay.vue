c8cf4b9d2ae70046a58377054431901706b8f05e
espadatiburon <espadatiburon@gmail.com>
2022-05-26 19:51:31 -0700

    GPUS-21

--- src/components/ReviewDisplay.vue
+++ src/components/ReviewDisplay.vue
@@ -7,6 +7,11 @@
             </b-row>
             <h4> {{item.title}} </h4>
             <p>{{item.text}}</p>
+            <b-row v-if="item.imgArray.length != 0">
+                <span v-for="img in item.imgArray" :key="img.imageid">
+                    <img :src="img.image" style="padding:25px">
+                </span>
+            </b-row>
         </b-card>
         <b-row class="justify-content-center">
             <b-pagination
@@ -40,6 +45,7 @@ export default {
     async mounted() 
     {  
         this.reviewsArray = await this.fetchReviews();
+        console.log(this.reviewsArray);
     },
     methods : 
     {
@@ -50,13 +56,44 @@ export default {
             const jsonFetchReviewsOutput = await this.performGetHttpRequest(apiUrl);
             console.log(jsonFetchReviewsOutput);
             if (jsonFetchReviewsOutput['status'] != 'SUCCESS') return [];
-            return jsonFetchReviewsOutput.resultset;
+            return await this.mapReviews(jsonFetchReviewsOutput.resultset);
         },
         formatDate(date)
         {
             const dateEndIndex = date.indexOf('T');
             return date.substr(0,dateEndIndex);
-        }
+        },
+        async mapReviews(reviewsList)
+        {
+            const reviewObject = {};
+            reviewsList.forEach(function(reviews)
+            {
+                const reviewid = reviews.reviewid;
+                const imgObject = {};
+                if(reviews.imageid) imgObject['imageid'] = reviews.imageid;
+                if(reviews.image) imgObject['image'] = reviews.image;
+                if(reviews.image_created) imgObject['created'] = reviews.image_created;
+                if(reviews.image_created_by) imgObject['created_by'] = reviews.image_created_by;
+                if(reviews.image_modified) imgObject['modified'] = reviews.image_modified;
+                if(reviews.image_modified_by) imgObject['modified_by'] = reviews.image_modified_by;
+
+                if(!reviewObject[reviewid]) 
+                {
+                    delete reviews.imageid;
+                    delete reviews.image;
+                    delete reviews.image_created;
+                    delete reviews.image_created_by;
+                    delete reviews.image_modified;
+                    delete reviews.image_modified_by;
+                    reviews.imgArray = [];
+                    reviewObject[reviewid] = reviews;
+                }
+
+                if(Object.keys(imgObject).length > 0) reviewObject[reviewid].imgArray.push(imgObject);
+            });
+
+            return Object.values(reviewObject);
+        },
     },
     computed : 
     {
