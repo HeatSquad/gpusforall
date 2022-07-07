b1b3d0219b428257a70638055e7d951bb0532bcf
Alex <aw23athas@gmail.com>
2022-07-06 21:23:53 -0700

    moved files to correct folder paths

---
+++ sys_webserver/src/views/ProductSearch.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <search-display
+        :searchString="searchString">
+        </search-display>
+    </div>
+</template>
+
+<script>
+import SearchDisplay from '../components/SearchDisplay.vue';
+
+export default {
+        components : {
+        SearchDisplay,
+    },
+    data() {
+        return {
+            searchString : ''
+        }
+    },
+    created(){}
+}
+</script>
+
+<style>
+
+</style>
